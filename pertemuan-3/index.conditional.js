8dae04aae4a38a392993cd59032bcab200b50679
Thoriq Nur Faizal <thoriiiq@gmail.com>
2021-01-25 16:23:00 +0700

    Adding materi pertemuan 1-3

---
+++ pertemuan-3/index.conditional.js
@@ -0,0 +1,54 @@
+// let lapar = false;
+
+// if (lapar) {
+//   console.log("makan kuy");
+// } else {
+//   console.log("nanti dulu");
+// }
+
+// contoh if else
+// let nilai = 79;
+
+// if (nilai === 100) {
+//   console.log("selamat nilai anda A");
+// } else if (nilai >= 90) {
+//   console.log("nilai anda B");
+// } else if (nilai >= 80) {
+//   console.log("nilai anda C");
+// } else {
+//   console.log("ANDA TIDAK LULUS");
+// }
+
+// let defaultName;
+
+// if (username) {
+//   username = defaultName;
+// } else {
+//   defaultName = "stranger";
+// }
+
+// let defaultName = username || "stranger";
+
+// contoh switch case
+
+// let nilai = 100;
+// switch (true) {
+//   case nilai === 100:
+//     console.log("selamat nilai anda A");
+//     break;
+//   case nilai >= 90:
+//     console.log("nilai anda B");
+//     break;
+//   default:
+//     console.log("nilai belum ada");
+//     break;
+// }
+
+// contoh ternary
+
+/*let nilai = 85;
+nilai === 100
+  ? console.log("selamat nilai anda A")
+  : nilai >= 90
+  ? console.log("nilai anda B")
+  : console.log("anda tidak lulus");*/
---
+++ pertemuan-3/index.js
@@ -0,0 +1,73 @@
+console.log("tesssss");
+
+let number1 = 1000;
+let number2 = 10.2;
+console.log("ini number 1", number1);
+console.log("ini number 2", number2);
+
+let string1 = "Nama saya draco ~!@$%^&*()__ 🎉";
+console.log("string1", string1);
+
+let angka = 1;
+let tulisan = "1";
+console.log(tulisan + angka);
+
+let benar = true;
+let salah = false;
+console.log(benar);
+console.log(salah);
+
+let null1 = null;
+let nullString = "";
+console.log(null1);
+console.log(nullString);
+
+let orang = {
+  nama: "draco",
+  umur: 50,
+  isMarried: false,
+};
+console.log(orang);
+console.log(orang.umur);
+console.log(orang.nama);
+
+let flowerRose = "mawar";
+let flowerTulip = "tulip";
+
+// let Boolean = "1";
+// console.log(Boolean);
+
+// number, string, boolean, object > num, str, bool, obj
+// camel case
+// nama lengkap >> namaLengkap
+// snake case
+// nama lengkap >> nama_lengkap
+
+// var vs let vs const
+// const num = 1;
+// console.log("num", num);
+
+// num = 10;
+// console.log("num2", num);
+
+var a = 1;
+{
+  var a = 2;
+  console.log("var 1", a);
+}
+
+console.log("var 2", a);
+
+let b = 1;
+{
+  let b = 2;
+  console.log("var 3", b);
+}
+
+console.log("var 4", b);
+
+let tambah = 10;
+// tambah += 1;
+tambah++;
+tambah++;
+console.log(tambah);
