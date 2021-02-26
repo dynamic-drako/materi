82913eb5ecd4bef20190b3ed7b2dfdf84ae28215
auzanassdq <auzanassidqi@gmail.com>
2021-02-26 09:09:47 +0700

    logout and protected route

--- pertemuan-13-register-login/index.js
+++ pertemuan-13-register-login/index.js
@@ -1,4 +1,10 @@
 let txtHeading = document.querySelector("#heading")
+let cart = document.querySelector("#cart")
+
+cart.addEventListener("click", () => {
+  let isLogin = localStorage.getItem("isLoggedin")
+  if (isLogin) window.location.href = "./cart.html"
+})
 
 let user = JSON.parse(localStorage.getItem("user"))
 if (user) {
