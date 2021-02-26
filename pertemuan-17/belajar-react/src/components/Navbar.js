0a4602185e2b8d0656a3521506b28f83b67b1364
auzanassdq <auzanassidqi@gmail.com>
2021-02-26 23:28:50 +0700

    react router dom

--- pertemuan-17/belajar-react/src/components/Navbar.js
+++ pertemuan-17/belajar-react/src/components/Navbar.js
@@ -4,7 +4,8 @@ function Navbar() {
   return (
     <div>
       <Link to="/">Home </Link>
-      <Link to="/profile">Profile</Link>
+      <Link to="/profile/Alpha">Profile</Link>
+      <Link to="/contact">Contact</Link>
     </div>
   );
 }
