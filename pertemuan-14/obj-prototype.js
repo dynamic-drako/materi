5505a6ae17d224da8c59ef3fa3004b1fdcd5bd49
auzanassdq <auzanassidqi@gmail.com>
2021-02-26 09:07:45 +0700

    obj prototype

---
+++ pertemuan-14/obj-prototype.js
@@ -0,0 +1,13 @@
+// OBJ Prototype
+String.prototype.getFirstChar = function () {
+  return this[0]
+}
+console.log("aplha".getFirstChar());
+
+// Func Biasa
+function getFirstCharBiasa (s) {
+  return s[0]
+}
+console.log(getFirstCharBiasa('alpha'));
+
+
